bceda18fe9e3c3910e67d92d6de01d1a948131de
Aline <alislima2013@gmail.com>
2025-03-18 00:01:27 -0300

    feat: add and update friends

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let friendsList = []
+
+function adicionarAmigo() {
+  let novoAmigo = document.querySelector('.input-name').value;
+
+  if (novoAmigo === '') {
+    alert("Por favor, insira um nome.")
+    return
+  }
+
+  friendsList.push(novoAmigo);
+  clearSpace()
+  updateList()
+}
+
+function clearSpace() {
+  let inputClear = document.querySelector("input");
+  inputClear.value = "";
+}
+
+function updateList() {
+  let newFriend = document.getElementById('listaAmigos')
+  newFriend.innerHTML = '';
+
+  friendsList.forEach((friend) => {
+    let li = document.createElement('li');
+    li.textContent = friend
+    newFriend.appendChild(li)
+  })
+}
+
+
